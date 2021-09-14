d0e0b00269ed75c7b172fef166623bacfa778939
satyam4853 <vaishnavsatyam48532gmail.com>
2021-09-15 00:22:11 +0530

    [satyam] Added:Welcome Message

---
+++ EmployeePayrollThreading/EmployeePayrollThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------This is EmployeePayroll Threading--------------------");
+        }
+    }
+}
