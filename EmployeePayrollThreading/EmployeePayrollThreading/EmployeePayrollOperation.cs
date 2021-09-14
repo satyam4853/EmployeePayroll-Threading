19e5b60d024d775530e8cac5f15fd86290ca2957
satyam4853 <vaishnavsatyam48532gmail.com>
2021-09-15 00:26:50 +0530

    [satyam] feat:Employee Data Added

---
+++ EmployeePayrollThreading/EmployeePayrollThreading/EmployeePayrollOperation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollThreading
+{
+  public class EmployeePayrollOperation
+    {
+        public List<EmployeeDetails> employeePayrollDetailsList = new List<EmployeeDetails>();
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDetailsList)
+        {
+            employeePayrollDetailsList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeeToPayroll(employeeData);
+                Console.WriteLine("Employee added: " + employeeData.EmployeeName);
+            });
+            Console.WriteLine(this.employeePayrollDetailsList.ToString());
+        }
+
+        public void addEmployeeToPayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailsList.Add(emp);
+        }
+    }
+}
